792e82e365810375ea0eacc597af4accef96e2d7
Daniell Mesquita <daniellplasmmer@gmail.com>
2019-07-08 23:16:51 -0400

    New line before saying Construct 2 project is identified, for organizing readability

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ ln -s /1/Floflis/libs/node_modules node_modules
 echo "- Installing NodeJS modules for this app..."
 sudo npm install -g
 if [ "$construct2" = "o" ]; then
+   echo ""
    echo "(!) Detected this app/game uses Construct 2 engine. Applying special patch for C2..."
    chmod 755 c2init.sh && ./c2init.sh
 fi
