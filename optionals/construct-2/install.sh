e0727c7b097e16a722d154252a7000f1426b5459
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-08 15:20:08 -0300

    Start adding initpreview and install.sh

---
+++ optionals/construct-2/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cat << "EOF"
+
+------------------------------
+-. .-.   .-. .-.   .-. .-.   .
+  \   \ /   \   \ /   \   \ /
+ / \   \   / \   \   / \   \
+~   `-~ `-`   `-~ `-`   `-~ `-
+  _            _           
+ |_  |   _   _|_  |  o   _ 
+ |   |  (_)   |   |  |  _> 
+                           
+        App patcher
+
+------------------------------
+
+EOF
+. ./config || exit 1
+echo "Linking NodeJS modules..."
+rm -rf node_modules
+ln -sf /1/Floflis/libs/node_modules node_modules
+#echo "Installing NodeJS modules for this app..."
+#npm install
+if [ "$construct2" = "o" ]; then
+   echo "Detected this app/game uses Construct 2 engine. Applying special patch for C2..."
+   chmod 755 c2init.sh && ./c2init.sh
+   echo "Done patching for Construct 2."
+fi
+echo "Done! Your app is ready."
